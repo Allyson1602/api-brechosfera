b360c732fac5e90200b1bbff554c7f5796fdfb16
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-12-17 17:04:02 -0300

    (feat): config to database and graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { LocationMapModule } from './location-map/location-map.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from 'postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    LocationMapModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
